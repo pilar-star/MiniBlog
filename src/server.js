bc87f787f62e9bb538b61c7eafb7e232222723a3
Pilar <pilarlugones0@gmail.com>
2026-05-11 17:47:04 -0300

    feat: se agregan valores y caracteristicas básicas en el server.js

--- src/server.js
+++ src/server.js
@@ -2,4 +2,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import router from "./routes/index.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(router);
+
+app.get("/", (req, res) => {
+    res.json({ message: "Bienvenio al MiniBlog API", version: "1.0"});
+});
+
+app.use((req, res) => {
+    res.status(404).json({ error: "Ruta no encontrada" });
+});
 
