70f21363643d161dbb2df3b4dc80e353bd6b5b29
Pilar <pilarlugones0@gmail.com>
2026-05-15 13:01:08 -0300

    feat se agregan más valores a la constante pool

--- src/db/config.js
+++ src/db/config.js
@@ -6,6 +6,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    max: 10, 
+    idleTimeoutMillis: 30000, 
+    connectionTimeoutMillis: 2000, 
 });
 
 export default pool;
