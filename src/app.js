a7defca426c9728f4c9d54ac9680a4ee73ae16df
Pilar <pilarlugones0@gmail.com>
2026-05-17 12:44:53 -0300

    fix: funciona la conexión al servidor

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import router from "./routes/index.js";
+import authorsRouter from "./routes/authorsRouter.js";
+import postsRouter from "./routes/postsRouter.js";
 const app = express();
+const Router = express.Router();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(router);
+app.use(Router);
 
 app.get("/", (req, res) => {
-    res.json({ message: "Bienvenio al MiniBlog API", version: "1.0"});
+    res.json({ message: "Bienvenido al MiniBlog API", version: "1.0"});
 });
+
+app.get("/authors", authorsRouter);
+app.get("/posts", postsRouter);
 
 app.use((req, res) => {
     res.status(404).json({ error: "Ruta no encontrada" });
@@ -16,4 +24,7 @@ app.use((err, req, res) => {
     res.status(500).json({ error: "Error interno del servidor" });
 });
 
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
 export default app;
