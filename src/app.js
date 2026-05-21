320b0d543af0059c5c6206323df7cd31098adb6c
Pilar <pilarlugones0@gmail.com>
2026-05-21 08:14:18 -0300

    feat: agrega ruta para manejo de errorees

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from 'express';
 import router from "./routes/index.js";
 import authorsRouter from "./routes/authorsRouter.js";
 import postsRouter from "./routes/postsRouter.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 const app = express();
 const Router = express.Router();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(errorHandler);
 app.use(Router);
 
 app.get("/", (req, res) => {
@@ -19,10 +21,7 @@ app.use("/posts", postsRouter);
 app.use((req, res) => {
     res.status(404).json({ error: "Ruta no encontrada" });
 });
-app.use((err, req, res) => {
-    console.error(err.stack);
-    res.status(500).json({ error: "Error interno del servidor" });
-});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
