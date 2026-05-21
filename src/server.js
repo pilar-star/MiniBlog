49c41efe7c27df6d1ee549e298c7ac97b8dc2fbd
Pilar <pilarlugones0@gmail.com>
2026-05-21 13:35:20 -0300

    se agregan rutas import para yaml y swagger (documentación e interfaz visual)

--- src/server.js
+++ src/server.js
@@ -4,5 +4,9 @@ import express from "express";
 import router from "./routes/index.js";
 import authorsRouter from "./routes/authorsRouter.js";
 import postsRouter from "./routes/postsRouter.js";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
 
+const swaggerDocument = YAML.load("./openapi.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const PORT = process.env.PORT || 3000;
